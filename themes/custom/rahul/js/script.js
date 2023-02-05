5cfb61e9903609a15c4eea30ef89b10e51f4903d
Rahul Khan <khanrahul.1011@gmail.com>
2023-02-05 14:36:39 +0530

    custom theme added

---
+++ themes/custom/rahul/js/script.js
@@ -0,0 +1,4 @@
+// Setting Current Year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = 'Copyright ' + currentYear + ' by Rahul Khan';
